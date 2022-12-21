4f0aa9b23af8c3ca88b6319ea7adbc88770fe9f4
manuelmenama <manuel.mena090589@gmail.com>
2022-12-21 16:05:35 +0100

    added some css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     <title>Primo Laravel</title>
 </head>
 <body>
 
-    <h1>Ciao</h1>
+    <div class="container">
+        <h1>Ciao</h1>
 
-    <a href="/landing">CLICCA QUI</a>
+        <a href="/landing">CLICCA QUI</a>
+    </div>
 
 </body>
 </html>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,27 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     <title>Landing page</title>
 </head>
 <body>
 
-    <h1>Operatori</h1>
+    <div class="container">
+        <h1>Operatori</h1>
 
 
-    <ul>
-        @foreach ($operators as $operator)
-            <li>
-                <ul>
-                    <li>
-                        {{ $operator['nome'] }}
-                    </li>
-                    <li>
-                        {{ $operator['cognome'] }}
-                    </li>
-                </ul>
-            </li>
-        @endforeach
-    </ul>
+        <ul>
+            @foreach ($operators as $operator)
+
+                <li>
+                    {{ $operator['nome'] }} {{ $operator['cognome'] }}
+                </li>
+
+            @endforeach
+        </ul>
+    </div>
 
 </body>
 </html>
